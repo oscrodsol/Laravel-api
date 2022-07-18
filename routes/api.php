4ffaf3425a9c80f0044f649ae759cb8356698ac2
oscrodsol <oscar43521@gmail.com>
2022-07-18 12:48:11 +0200

    feat: create routes user

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,22 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::get('/',function (){
+    return 'Bienvenido a mi app';
+});
+
+Route::get('/users',function (){
+    return 'get';
+});
+
+Route::post('/users',function (){
+    return ['post'];
+});
+
+Route::put('/users',function (){
+    return ['put'];
+});
+
+Route::delete('/users',function (){
+    return ['delete'];
 });
